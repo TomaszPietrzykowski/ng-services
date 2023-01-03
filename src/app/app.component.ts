c4ee7fae88c73a06f7f26be900a737347ebf9dff
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2023-01-03 15:28:26 +0100

    data passed to template using async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,34 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import {
+  // AfterViewInit,
+  Component,
+  // ElementRef,
+  OnInit,
+  // QueryList,
+  // ViewChild,
+  // ViewChildren,
+} from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+// import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+// import { CourseCardComponent } from "./course-card/course-card.component";
+// import { HighlightedDirective } from "./directives/highlighted.directive";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-
-  courses = COURSES;
-
-  constructor() {
-
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    // HttpParams has immutability based api - to change use .set() method
+    const params = new HttpParams().set("page", "1").set("pageSize", "10");
+
+    this.courses$ = this.http.get<Course[]>("/api/courses", { params });
   }
-
-
-
 }
